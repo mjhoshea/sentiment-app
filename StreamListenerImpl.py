deefdebefa04b2fd9606bf6798faa10f52b9597d
mjhoshea <michael@simudyne.com>
2020-04-13 14:28:55 +0100

    analyse twitter tweets using nltk vader

--- StreamListenerImpl.py
+++ StreamListenerImpl.py
@@ -6,9 +6,18 @@ class StreamListenerImpl(StreamListener):
     def __init__(self, producer):
         super().__init__()
         self.producer = producer
+        self.contin = True
 
     def on_data(self, raw_data):
-        self.producer.send('test', json.loads(raw_data)['text'])
+        if(self.contin == False):
+            return False
+        self.producer.send('test', raw_data)
 
     def on_status(self, status):
         print(status.text)
+
+    def stop(self):
+        self.contin = False
+
+    def start(self):
+        self.contin = True
--- app.py
+++ app.py
@@ -3,8 +3,17 @@ import tweepy
 from StreamListenerImpl import StreamListenerImpl
 from kafka import KafkaProducer, KafkaConsumer
 import json
+import nltk
+
+
 
 app = Flask(__name__)
+
+nltk.download('vader_lexicon')
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sid = SentimentIntensityAnalyzer()
 
 consumer_key='yZst27GNzlTDMVnxw5qKPTp53'
 consumer_secret='1Mh6ZqmZ042YAjeyf5cZujGbY99NrtTuvP5lVgX3T0OAoj7Sis'
@@ -16,23 +25,41 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-consumer = KafkaCo
+
 streamListener = StreamListenerImpl(producer)
 myStream = tweepy.Stream(auth=api.auth, listener=streamListener)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return 'To begin analysing a topic go to localhost:5000/track/<topic>'
+
 
 @app.route('/track/<topic>')
-def hello_boris(topic):
-    print(topic)
+def track_topic(topic):
+    streamListener.start()
     myStream.filter(track=[topic], is_async=True)
     return 'Getting Your Topic'
 
+@app.route('/analysis/<topic>')
+def analysis_topic(topic):
+    print(topic)
+    consumer = KafkaConsumer('test', bootstrap_servers='localhost:9092', value_deserializer=lambda m: json.loads(m.decode('utf-8')))
+    for mes in consumer:
+        tweet = json.loads(mes.value)['text']
+        print('                         ')
+        print('-------------------------')
+        print(tweet)
+        print(sid.polarity_scores(tweet))
+        print('-------------------------')
+        print('                         ')
+
+
+@app.route('/stop')
+def stop_stream():
+    streamListener.stop()
+    return 'Stopping your stream'
+
+
 if __name__ == '__main__':
-
-
-
-    app.run()
+    app.run(host='0.0.0.0')
